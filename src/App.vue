d04926cba12a69f0f143c18179eb5226d342cbc8
evasyukov <egor.vasyukov1@gmail.com>
2024-07-19 14:36:40 +0700

    create project

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div style="width: 800px; height: 200px;">
+      <TagsList :tags="tags" />
+    </div>
+
+    <v-btn @click="addTag">Добавить тег</v-btn>
+  </div>
+</template>
+
+<script>
+import TagsList from "./components/TagsList.vue";
+
+export default {
+  name: "App",
+  components: {
+    TagsList,
+  },
+  data() {
+    return {
+      tags: [
+        { text: "v-btn" },
+        { text: "текст2", icon: "mdi-check" },
+        { text: "текст1", icon: "mdi-check" },
+        { text: "текст2", icon: "mdi-check" },
+        { text: "текст1" },
+        { text: "текст2", icon: "mdi-check" },
+      ],
+    };
+  },
+  methods: {
+    addTag() {
+      this.tags.push({ text: "Новый тег", icon: "mdi-plus" });
+    },
+    removeTag(index) {
+      this.tags.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+  height: 100%;
+}
+</style>
---
+++ src/components/TagsList.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="tags-list">
+      <div v-for="(tag, index) in tags" :key="index" >
+        
+        {{ tag.text }}
+        <v-icon v-if="tag.icon" class="tag-icon">{{ tag.icon }}</v-icon>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      align: String,
+      tags: Array
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .tags-list {
+    margin-bottom: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  
+    .tag-item {
+      display: inline-flex;
+      align-items: center;
+      padding: 8px 12px;
+      background-color: #e0e0e0;
+      border-radius: 16px;
+      margin-right: 10px;
+      transition: background-color 0.3s;
+  
+      &.align-left {
+        justify-content: flex-start;
+      }
+  
+      &.align-center {
+        justify-content: center;
+      }
+  
+      &:hover {
+        background-color: darken(#e0e0e0, 5%);
+      }
+  
+      .tag-icon {
+        margin-left: 6px;
+      }
+    }
+  }
+  </style>
+  
